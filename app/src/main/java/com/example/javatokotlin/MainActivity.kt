00c4d9244dd652b896c10efd6c575709080b7039
Chaiwat Luecha <chaiwat.lue@mtel.co.th>
2019-07-22 01:22:32 +0700

    init

--- app/src/main/java/com/example/javatokotlin/MainActivity.kt
+++ app/src/main/java/com/example/javatokotlin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.javatokotlin
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
@@ -15,11 +17,22 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val FIRST_RANGE_COUNTER = "first_range_counter"
+        const val SECOND_RANGE_COUNTER = "second_range_counter"
+        const val THIRD_RANGE_COUNTER = "third_range_counter"
+        const val FOURTH_RANGE_COUNTER = "fourth_range_counter"
+        const val FIFTH_RANGE_COUNTER = "fifth_range_counter"
+        const val OTHER_COUNTER = "other_counter"
+    }
+
     private val defaultNumber: String by lazy { randomInt(50).toString() }
+    private lateinit var sharePreference: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharePreference = PreferenceManager.getDefaultSharedPreferences(this)
         initUi()
     }
 
@@ -68,21 +81,38 @@ class MainActivity : AppCompatActivity() {
     private fun updateBackgroundColor(number: Int) {
         constraintLayout.setBackgroundColor(when (number) {
             in 1..10 -> {
-                getColorCompat(android.R.color.black)
+                increaseCounter(FIRST_RANGE_COUNTER)
+                getColorCompat(android.R.color.holo_red_dark)
+            }
+            in 11..20 -> {
+                increaseCounter(SECOND_RANGE_COUNTER)
+                getColorCompat(android.R.color.holo_green_dark)
             }
             in 21..30 -> {
+                increaseCounter(THIRD_RANGE_COUNTER)
                 getColorCompat(android.R.color.holo_blue_dark)
             }
             in 31..40 -> {
+                increaseCounter(FOURTH_RANGE_COUNTER)
                 getColorCompat(android.R.color.holo_orange_dark)
             }
             in 41..50 -> {
+                increaseCounter(FIFTH_RANGE_COUNTER)
                 getColorCompat(android.R.color.holo_purple)
             }
             else -> {
+                increaseCounter(OTHER_COUNTER)
                 getColorCompat(android.R.color.black)
             }
         })
     }
 
+    private fun increaseCounter(key: String) {
+        with(sharePreference) {
+            var counter = getInt(key, 0)
+            counter++
+            edit().putInt(key, counter).apply()
+        }
+
+    }
 }
--- app/src/main/java/com/example/javatokotlin/SecondActivity.kt
+++ app/src/main/java/com/example/javatokotlin/SecondActivity.kt
@@ -2,12 +2,33 @@ package com.example.javatokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.javatokotlin.R
+import android.preference.PreferenceManager
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    private val sharePreference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        with(sharePreference) {
+            firstRangeTextView.text = getInt(MainActivity.FIRST_RANGE_COUNTER, 0).toString()
+            secondRangeTextView.text = getInt(MainActivity.SECOND_RANGE_COUNTER, 0).toString()
+            thirdRangeTextView.text = getInt(MainActivity.THIRD_RANGE_COUNTER, 0).toString()
+            fourthRangeTextView.text = getInt(MainActivity.FOURTH_RANGE_COUNTER, 0).toString()
+            fifthRangeTextView.text = getInt(MainActivity.FIFTH_RANGE_COUNTER, 0).toString()
+            otherRangeTextView.text = getInt(MainActivity.OTHER_COUNTER, 0).toString()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
